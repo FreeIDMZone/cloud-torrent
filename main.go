52fb884f66e820b513217bef4bb3ff295fa50615
pswalia2u <pswalia2u@gmail.com>
2016-12-04 11:04:30 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"github.com/jpillora/opts"
 )
 
-var VERSION = "0.0.0-src" //set with ldflags
+var VERSION = "1.1.3-src" //set with ldflags
 
 func main() {
 	s := server.Server{
-		Title:      "Cloud Torrent",
+		Title:      "NKR's Torrent Leecher",
 		Port:       3000,
 		ConfigPath: "cloud-torrent.json",
 	}
